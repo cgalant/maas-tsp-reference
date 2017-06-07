381f00f8bdc8122d9ce74cdaaa39526f97f7eaf3
cgalant <christophe.galant@gmail.com>
2017-06-07 14:42:13 +0200

    [BUILD] Update package and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,10 @@ const eslint = require('gulp-eslint');
 const gmocha = require('gulp-mocha');
 const gulpSequence = require('gulp-sequence');
 const gutil = require('gulp-util');
-const spawn = require('child_process').spawn;
 
 const jsoncFiles = ['.eslintrc']; // json with comments
 const jsonFiles = ['**/*.json', '!**/node_modules/**/*.json', '!_meta/**/*.json', '!schemas/**/*.json'];
 const jsFiles = ['**/*.js', '!**/node_modules/**/*.js', '!_meta/**/*.js', '!schemas/**/*.js'];
-
-// Run sls offline in background while performing mocha tests
-let localServer;
 
 gulp.task('jsonclint', () => {
 
@@ -41,52 +37,20 @@ gulp.task('eslint', () => {
     .pipe(eslint.failAfterError());
 });
 
-// Local test server config
-gulp.task('serverStart', cb => {
-  localServer = spawn('sls', ['offline', 'start']
-  //,{detached: true, stdio: 'ignore'}
-  //,{stdio: 'inherit', shell: true} // FOR WINDOWS OS
-  );
-  localServer.on('error', err => {
-    console.error(err);
-    throw err;
-  });
-
-  /* */
-  localServer.stderr.on('data', data => {
-    console.log('stderr: ' + data);
-  });
-  localServer.stdout.on('data', chunk => {
-    if (chunk.toString().indexOf('listening on') > -1) {
-      console.log(chunk.toString());
-      cb();
-    }
-  });
-
-
-  localServer.on('close', code => {
-    console.log('child process exited with code ' + code);
-  });
-});
-
-gulp.task('serverStop', () => {
-  localServer.kill('SIGTERM');
-  console.log('Stopped Serverless Offline!');
-  process.exit();
-});
-
 gulp.task('mocha', () => {
   return gulp.src('tests/test.js', { read: false })
     .pipe(gmocha({ grep: argv.grep }))
     .on('error', error => {
       gutil.log(error);
-      localServer.kill('SIGTERM');
       process.exit(1);
+    })
+    .on('end', () => {
+      process.exit();
     });
 });
 
 gulp.task('validate', ['jsonclint', 'jsonlint', 'eslint']);
 
-gulp.task('test', ['validate'], gulpSequence('serverStart', 'mocha', 'serverStop'));
+gulp.task('test', gulpSequence('validate', 'mocha'));
 
 gulp.task('default');
